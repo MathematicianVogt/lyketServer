e85ffbc7743be87721aa1c2a5f379fbbdc192a18
Emma Foster <deafjams@gmail.com>
2015-10-11 13:28:07 -0400

    Example with variable in URL, JSON return

--- server.py
+++ server.py
@@ -5,10 +5,18 @@ import os
 class HomeHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render(os.path.join(os.getcwd(), 'static', 'index.html'))
+		
+class ExampleHandler(tornado.web.RequestHandler):
+	
+	def get(self, variable_name=''):
+		
+		self.write({'results': variable_name})
+		
 
 def main():
 	app = tornado.web.Application([
 		tornado.web.url(r'/static/(.*)', tornado.web.StaticFileHandler, {'path': os.path.join(os.getcwd(), 'static')}),
+		tornado.web.url(r'/example/?(?P<variable_name>.+)?', ExampleHandler),
 		tornado.web.url(r'/', HomeHandler)
 	])
 	app.listen(8888)
