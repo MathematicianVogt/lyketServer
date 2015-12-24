7ed8736f1a21558a2e895fae73cd50397ce2ca3e
Emma Foster <deafjams@gmail.com>
2015-12-24 00:57:24 -0500

    whitespace fixes

--- server.py
+++ server.py
@@ -5,50 +5,50 @@ import pymongo
 import bson
 
 class HomeHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render(os.path.join(os.getcwd(), 'static', 'modules', 'lyket', 'lyket.html'))
+    def get(self):
+        self.render(os.path.join(os.getcwd(), 'static', 'modules', 'lyket', 'lyket.html'))
 
 class ArticleHandler(tornado.web.RequestHandler):
-	def get(self):
-		
-		db = pymongo.MongoClient()
-		
-		articles = list(db.lyket.articles.find().limit(10))
-		
-		for article in articles:
-			article['_id'] = str(article['_id'])
+    def get(self):
+
+        db = pymongo.MongoClient()
+
+        articles = list(db.lyket.articles.find().limit(10))
+
+        for article in articles:
+            article['_id'] = str(article['_id'])
             article['creationtime'] = article['creationtime'].isoformat()
-		
-		self.write({'results': articles})
-		
+
+        self.write({'results': articles})
+
 class LykeHandler(tornado.web.RequestHandler):
-	def get(self, article_id=''):
-		
-		db = pymongo.MongoClient()
-		
-		db.lyket.articles.update({'_id': bson.objectid.ObjectId(article_id)}, {'$inc': {'likes': 1}})
-		
-		self.write('successful')
-		
+    def get(self, article_id=''):
+
+        db = pymongo.MongoClient()
+
+        db.lyket.articles.update({'_id': bson.objectid.ObjectId(article_id)}, {'$inc': {'likes': 1}})
+
+        self.write('successful')
+
 class DislykeHandler(tornado.web.RequestHandler):
-	def get(self, article_id=''):
-		
-		db = pymongo.MongoClient()
-		
-		db.lyket.articles.update({'_id': bson.objectid.ObjectId(article_id)}, {'$inc': {'dislikes': 1}})
-		
-		self.write('successful')
+    def get(self, article_id=''):
+
+        db = pymongo.MongoClient()
+
+        db.lyket.articles.update({'_id': bson.objectid.ObjectId(article_id)}, {'$inc': {'dislikes': 1}})
+
+        self.write('successful')
 
 def main():
-	app = tornado.web.Application([
-		tornado.web.url(r'/lyke/(?P<article_id>.+)', LykeHandler),
-		tornado.web.url(r'/dislyke/(?P<article_id>.+)', DislykeHandler),
-		tornado.web.url(r'/articles', ArticleHandler),
-		tornado.web.url(r'/static/(.*)', tornado.web.StaticFileHandler, {'path': os.path.join(os.getcwd(), 'static')}),
-		tornado.web.url(r'/', HomeHandler)
-	], debug=True)
-	app.listen(8000)
-	tornado.ioloop.IOLoop.current().start()
-	
+    app = tornado.web.Application([
+            tornado.web.url(r'/lyke/(?P<article_id>.+)', LykeHandler),
+            tornado.web.url(r'/dislyke/(?P<article_id>.+)', DislykeHandler),
+            tornado.web.url(r'/articles', ArticleHandler),
+            tornado.web.url(r'/static/(.*)', tornado.web.StaticFileHandler, {'path': os.path.join(os.getcwd(), 'static')}),
+            tornado.web.url(r'/', HomeHandler)
+    ], debug=True)
+    app.listen(8000)
+    tornado.ioloop.IOLoop.current().start()
+
 if __name__ == '__main__':
-	main()
+    main()
