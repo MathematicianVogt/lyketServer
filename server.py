dbd05f847bebfe41da4c2525cddf70d0252e07e5
Emma Foster <deafjams@gmail.com>
2016-01-03 17:44:27 -0500

    Most recent articles handler

--- server.py
+++ server.py
@@ -1,19 +1,29 @@
 import tornado.ioloop
 import tornado.web
 import os
-import pymongo
+import motor
 import bson
 
 class HomeHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render(os.path.join(os.getcwd(), 'static', 'modules', 'lyket', 'lyket.html'))
+        self.render(
+            os.path.join(
+                os.getcwd(), 
+                'static', 
+                'modules', 
+                'lyket', 
+                'lyket.html'
+            )
+        )
 
 class ArticleHandler(tornado.web.RequestHandler):
+    @tornado.gen.coroutine
     def get(self):
 
-        db = pymongo.MongoClient()
+        database = self.settings['db']
 
-        articles = list(db.lyket.articles.find().limit(10))
+        articles = database.lyket.articles.find().sort('creationtime', -1).limit(10)
+        articles = yield articles.to_list(10)
 
         for article in articles:
             article['_id'] = str(article['_id'])
@@ -26,7 +36,10 @@ class LykeHandler(tornado.web.RequestHandler):
 
         db = pymongo.MongoClient()
 
-        db.lyket.articles.update({'_id': bson.objectid.ObjectId(article_id)}, {'$inc': {'likes': 1}})
+        db.lyket.articles.update({
+            '_id': bson.objectid.ObjectId(article_id)},
+            {'$inc': {'likes': 1}
+        })
 
         self.write('successful')
 
@@ -35,18 +48,31 @@ class DislykeHandler(tornado.web.RequestHandler):
 
         db = pymongo.MongoClient()
 
-        db.lyket.articles.update({'_id': bson.objectid.ObjectId(article_id)}, {'$inc': {'dislikes': 1}})
+        db.lyket.articles.update({
+            '_id': bson.objectid.ObjectId(article_id)},
+            {'$inc': {'dislikes': 1}
+        })
 
         self.write('successful')
 
 def main():
+
+    database = motor.motor_tornado.MotorClient()
+    database.lyket.articles.create_index(
+        'creationtime',
+        backgreat=True
+    )
+
     app = tornado.web.Application([
             tornado.web.url(r'/lyke/(?P<article_id>.+)', LykeHandler),
             tornado.web.url(r'/dislyke/(?P<article_id>.+)', DislykeHandler),
             tornado.web.url(r'/articles', ArticleHandler),
             tornado.web.url(r'/static/(.*)', tornado.web.StaticFileHandler, {'path': os.path.join(os.getcwd(), 'static')}),
             tornado.web.url(r'/', HomeHandler)
-    ], debug=True)
+        ],
+        db=database,
+        debug=True
+    )
     app.listen(8000)
     tornado.ioloop.IOLoop.current().start()
 
